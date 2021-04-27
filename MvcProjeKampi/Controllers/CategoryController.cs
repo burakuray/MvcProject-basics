3bfcce36c65ea18fe8d8dfa0a177114f064acb16
b.uray <burak.uray@gmail.com>
2021-04-28 01:46:26 +0300

    Controller & new views added

---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using EntitiyLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Caregory
+        CategoryManager cm = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList() 
+        {
+            var categoryValues = cm.GetAllBL();
+
+            return View(categoryValues);
+        }
+
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
